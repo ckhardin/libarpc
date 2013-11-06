6bfd84584f80b96360d975055ecf20a98e7527fd
Charles Hardin <ckhardin@gmail.com>
2013-11-05 19:32:16 -0800

    arpcgen: fix the build on mac os x
    The compat.h wasn't being included in the build correctly
    and so the HAVE_ definitions were not being handled

--- arpcgen/rpc_main.c
+++ arpcgen/rpc_main.c
@@ -35,7 +35,7 @@ static char sccsid[] = "@(#)rpc_main.c 1.30 89/03/30 (C) 1987 SMI";
 #endif
 #endif
 
-#include <sys/cdefs.h>
+#include "compat.h"
 
 /*
  * rpc_main.c, Top level of the RPC protocol compiler.
